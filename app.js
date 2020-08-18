d2577dbe03f79433bcb688e9132599d96524a09f
Dan Blustein <dansakai0@gmail.com>
2020-08-19 02:36:38 +0900

    Implemented functionality of patching player elos

--- app.js
+++ app.js
@@ -14,15 +14,8 @@ mongoose.connection.on("connected", () => {
     console.log("Connection to database successful")
 })
 
-app.get('/', (req, res) => {
-    res.json({
-        confirmation: "success"
-    })
-})
-
-app.get('/api', (req, res) => {
+app.get('/api/players', (req, res) => {
     const query = req.query
-
     if (query.min && query.max) { // returns players that are in a range of elo values
         Player.find({goatElo: { $gte: query.min, $lte: query.max }})
             .then((players) => {
@@ -54,8 +47,77 @@ app.get('/api', (req, res) => {
                 })
             })
         })
+    } else { // simply return all players
+        Player.find()
+            .then((players) => {
+                res.json({
+                    players
+                })
+            })
     }
 })
 
+app.patch('/api/players', async (req, res) => {
+    const query = req.query
+    if (query._idOne && query._idTwo && query.winner) {
+        
+        let playerOne = false
+        let playerTwo = false
+        
+        try {
+            const playerOneData = await Player.findById(query._idOne)
+            const playerTwoData = await Player.findById(query._idTwo)
+            playerOne = playerOneData
+            playerTwo = playerTwoData
+        } catch (err) {
+            res.json({
+                message: err
+            })
+        }
 
+        if (playerOne && playerTwo) {
+            // adjustEloRatings(playerOne, playerTwo, query.winner === "1" ? true : false)
+            // await Player.updateOne({_id: playerOne._id}, {$set: {goatElo: playerOne.goatElo}})
+            // await Player.updateOne({_id: playerTwo._id}, {$set: {goatElo: playerTwo.goatElo}})
+        } else {
+            res.json({
+                message: "Error in retrieving players."
+            })
+        }
+
+        res.json({
+            playerOne: playerOne,
+            playerTwo: playerTwo
+        })
+    }
+})
+
+/*
+  MODIFIES: The elo ratings of player1 and player2
+  EFFECTS: Adjusts the elo ratings of player1 & player2
+  depending on the truth value of player1Wins
+  Notes:
+  TR = Transformed Rating
+  ES = Expected Score
+  AER = Adjusted Elo Rating
+*/
+function adjustEloRatings(player1, player2, player1Wins) {
+    const player1TR = Math.pow(10, player1.goatElo / 400)
+    const player2TR = Math.pow(10, player2.goatElo / 400)
+  
+    const player1ES = player1TR / (player1TR + player2TR)
+    const player2ES = player2TR / (player1TR + player2TR)
+  
+    const player1Score = player1Wins ? 1 : 0
+    const player2Score = player1Wins ? 0 : 1
+  
+    const kValue = 32
+    const player1AER = player1.goatElo + kValue * (player1Score - player1ES)
+    const player2AER = player2.goatElo + kValue * (player2Score - player2ES)
+  
+    player1.goatElo = player1AER
+    player2.goatElo = player2AER
+  }
+
+  
 app.listen(3000);
